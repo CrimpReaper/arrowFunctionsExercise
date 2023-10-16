10695a2fc8c26e714768f52a34275195dcd6b9d8
Greg Thoma <CrimpReaper@gmail.com>
2023-10-15 21:41:18 -0400

    sorry.. now first problem solved

---
+++ app.js
@@ -0,0 +1,10 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+
+//Refactor the above code to use two arrow functions. Turn it into a one-liner.
+
+const double = (arr) => {return arr.map((val) => {return val * 2;});}
