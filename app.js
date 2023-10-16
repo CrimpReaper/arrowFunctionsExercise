82915b8e4ce08c3c4ab523e27bfcf1279e7a0b8f
Greg Thoma <CrimpReaper@gmail.com>
2023-10-15 21:46:55 -0400

    second problem solved

--- app.js
+++ app.js
@@ -7,4 +7,29 @@
 
 //Refactor the above code to use two arrow functions. Turn it into a one-liner.
 
-const double = (arr) => {return arr.map((val) => {return val * 2;});}
+ const double = (arr) => {return arr.map((val) => {return val * 2;});}
+
+
+// ## **Refactor the following function to use arrow functions:**
+
+// Replace ALL functions with arrow functions:
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+  
+const squareAndFindEvens = (numbers) => {
+    let squares = numbers.map((num) => {
+        return num ** 2;
+    });
+
+    let evens = squares.filter((square) => {
+        return square % 2 === 0;
+    });
+    return evens;
+}
